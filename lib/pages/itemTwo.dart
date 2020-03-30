f01ba7b2b49b7dc96900a32ae68589c934131730
RexMud <rex@limatech.co.ke>
2020-03-30 09:57:09 +0300

    added random color to circle avatar

--- lib/pages/itemTwo.dart
+++ lib/pages/itemTwo.dart
@@ -29,6 +29,17 @@ class _ItemTwoState extends State<ItemTwo> {
     });
   }
 
+  List<MaterialColor> colorItem = [
+    Colors.deepOrange,
+    Colors.amber,
+    Colors.pink,
+    Colors.purple,
+    Colors.yellow,
+    Colors.lightBlue,
+    Colors.pink
+  ];
+  MaterialColor color;
+
 
   @override
   Widget build(BuildContext context) {
@@ -48,14 +59,86 @@ class _ItemTwoState extends State<ItemTwo> {
             child: ListView.builder(
               itemCount: snapshot.data.length,
               itemBuilder: (context, index){
-                return Card(
-                  elevation: 10,
-                  margin: EdgeInsets.all(10),
-                  child: Container(
+
+                var ourdata = snapshot.data[index];
+
+                color = colorItem[index % colorItem.length];
+
+                return Container(
+                 height: 420,
+                  margin: EdgeInsets.all(10.0),
+                  child: Card(
+                    elevation: 10.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
                     child: Column(
                       children: <Widget>[
+                        // first container
+                        Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Container(
+                                child: Row(
+                                  children: <Widget>[
+                                    Container(
+                                      margin: EdgeInsets.all(10.0),
+                                      child: CircleAvatar(
+                                        child: Text(ourdata.data["title"][0], style: TextStyle(fontSize: 20.0),),
+                                        backgroundColor: color,
+                                      ),
+                                    ),
 
-                        Text(snapshot.data[index].data["title"])
+                                    SizedBox(width: 5,),
+                                    Container(
+                                      child: Text(ourdata.data['title'],
+                                        style: TextStyle(
+                                          fontSize: 20.0,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+
+
+                              Container(
+                                margin: EdgeInsets.only(right: 20),
+                                child: Icon(Icons.more_horiz, size: 35,),
+                              )
+
+                            ],
+                          ),
+                        ),
+
+                        SizedBox(height: 10,),
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.network(ourdata.data['image'],
+                            height: 200,
+                              width: MediaQuery.of(context).size.width,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 8.0,),
+
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          child: Text(ourdata.data['desc'],
+                          maxLines: 4,
+                            style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.black
+                          ),
+                          ),
+                        )
+
                       ],
                     ),
                   ),
