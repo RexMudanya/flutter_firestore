831d8feddfc574c176d6c7914caf77a8dec79c71
RexMud <rex@limatech.co.ke>
2020-04-19 16:01:48 +0300

    added gridview

--- lib/pages/itemThree.dart
+++ lib/pages/itemThree.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/rendering.dart';
 
 class ItemThree extends StatefulWidget {
   @override
@@ -6,8 +9,54 @@ class ItemThree extends StatefulWidget {
 }
 
 class _ItemThreeState extends State<ItemThree> {
+
+  Future getGridView() async{
+
+    var firestore = Firestore.instance;
+
+    QuerySnapshot snap = await firestore.collection("gridData").getDocuments();
+
+    return snap.documents;
+
+  }
+
+  Future<Null>getRefresh() async{
+
+    await Future.delayed(Duration(seconds: 3));
+    setState(() {
+      getGridView();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return new Scaffold(
+      body: FutureBuilder(
+          future: getGridView(),
+          builder: (context, snapshot){
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return Center(child: CircularProgressIndicator(),);
+            }else{
+              return RefreshIndicator(
+                  child: GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (context, index){
+                        var ourData = snapshot.data[index];
+                        return Card(
+                          elevation: 10,
+                          shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(20.0)),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                              child: Image.network(ourData.data['image'], fit: BoxFit.cover,),
+                          ),
+                        );
+                      }),
+                  onRefresh: getRefresh);
+            }
+          },
+      ),
+
+    ); //scaffold
   }
 }
